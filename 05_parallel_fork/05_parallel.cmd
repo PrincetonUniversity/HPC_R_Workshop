c8b9ec33e60705f0138f3685c891c5a4852a8771
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:32:15 -0500

    Update 05_parallel.cmd

--- 05_parallel_fork/05_parallel.cmd
+++ 05_parallel_fork/05_parallel.cmd
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=R-fork        # create a short name for your job
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks=10              # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=10       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on start, end and fault
