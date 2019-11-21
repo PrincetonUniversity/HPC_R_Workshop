081f5e63fc81e402f354fb2bd85f14d74c4e004f
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:35:01 -0500

    Update 06_single_core.cmd

--- 06_MKL_as_BLAS/06_single_core.cmd
+++ 06_MKL_as_BLAS/06_single_core.cmd
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 05:00
-# This uses the class reservation and will only work for our 
-# class today, in the future, remove this from your SBATCH
-# SBATCH -p class
+#SBATCH --job-name=R-mkl         # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-user=<YourNetID>@princeton.edu
 
-/usr/bin/Rscript test.R
-
+srun Rscript test.R
