b687dcb691a6b504752edfc6507ec9b5a53c58e0
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:37:08 -0500

    Update 06_MKL.cmd

--- 06_MKL_as_BLAS/06_MKL.cmd
+++ 06_MKL_as_BLAS/06_MKL.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=R-mkl        # create a short name for your job
+#SBATCH --job-name=R-mkl         # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
