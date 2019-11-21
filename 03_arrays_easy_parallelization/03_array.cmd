109f37b01e12c5fbce034736dbce218a96e07fac
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:13:00 -0500

    Update 03_array.cmd

--- 03_arrays_easy_parallelization/03_array.cmd
+++ 03_arrays_easy_parallelization/03_array.cmd
@@ -1,14 +1,12 @@
 #!/bin/bash
-# array job using 1 nodes and 1 processor,
-# and runs for five minutes max per task.
-#SBATCH -N 1   # node count
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 00:05:00
-#SBATCH --array=0-5
+#SBATCH --job-name=R-serial      # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --array=0-5              # array job with task id's 0, 1, 2, 3, 4, 5
+#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-user=<YourNetID>@princeton.edu
 
-# This uses the class reservation and will only work for our 
-# class today, in the future, remove this from your SBATCH
-# SBATCH -p class
-
-
-Rscript array.R
+srun Rscript array.R
