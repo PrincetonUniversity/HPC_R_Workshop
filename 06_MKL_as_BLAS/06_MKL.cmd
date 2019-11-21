8a24772ebde25a0b38303c86e1259be76af75e14
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:36:56 -0500

    Update 06_MKL.cmd

--- 06_MKL_as_BLAS/06_MKL.cmd
+++ 06_MKL_as_BLAS/06_MKL.cmd
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 2 
-#SBATCH -t 05:00
-
-# This uses the class reservation and will only work for our 
-# class today, in the future, remove this from your SBATCH
-# SBATCH -p class
+#SBATCH --job-name=R-mkl        # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-user=<YourNetID>@princeton.edu
 
 # This sets the intel-mkl to use the GNU interface, which 
 # is more compatible with R
@@ -14,5 +14,4 @@ export  MKL_INTERFACE_LAYER=GNU,LP64
 export  MKL_THREADING_LAYER=GNU
 
 module load intel-mkl
-LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_rt.so /usr/bin/Rscript test.R
-
+LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_rt.so Rscript test.R
