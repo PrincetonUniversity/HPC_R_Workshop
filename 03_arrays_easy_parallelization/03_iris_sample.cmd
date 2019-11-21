f9a152441373a3dc5934e7fccf698517c53b18d4
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:21:50 -0500

    Update 03_iris_sample.cmd

--- 03_arrays_easy_parallelization/03_iris_sample.cmd
+++ 03_arrays_easy_parallelization/03_iris_sample.cmd
@@ -1,13 +1,12 @@
 #!/bin/bash
-# array job using 1 nodes and 1 processor,
-# and runs for five minutes max per task.
-#SBATCH -N 1   # node count
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 00:05:00
-#SBATCH --array=1-3
+#SBATCH --job-name=R-iris        # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --array=1-3              # job array with index values 1, 2, 3
+#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-user=<YourNetID>@princeton.edu
 
-# This uses the class reservation and will only work for our 
-# class today, in the future, remove this from your SBATCH
-# SBATCH -p class
-
-Rscript 03_iris.R $SLURM_ARRAY_TASK_ID 
+srun Rscript 03_iris.R $SLURM_ARRAY_TASK_ID
