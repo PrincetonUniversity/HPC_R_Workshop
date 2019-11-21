3b30a130d3a965884a1e1623fd8cea28da121454
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:16:26 -0500

    Update 03_array.cmd

--- 03_arrays_easy_parallelization/03_array.cmd
+++ 03_arrays_easy_parallelization/03_array.cmd
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
-#SBATCH --array=0-5              # job array with index values 0, 1, 2 ,3 , 4, 5
+#SBATCH --array=0-5              # job array with index values 0, 1, 2, 3, 4, 5
 #SBATCH --mail-type=all          # send email on start, end and fault
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
