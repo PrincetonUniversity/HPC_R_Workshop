98388f85fa151ad825f66bd463909b77fe14a37f
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:30:40 -0500

    Update 05_parallel.cmd

--- 05_parallel_fork/05_parallel.cmd
+++ 05_parallel_fork/05_parallel.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=R-serial      # create a short name for your job
+#SBATCH --job-name=R-fork        # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=10              # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
