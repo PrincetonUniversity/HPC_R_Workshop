676848f872b5352b8b696892bf87900b28fc9e56
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-21 16:28:53 -0500

    Update 05_parallel.cmd

--- 05_parallel_fork/05_parallel.cmd
+++ 05_parallel_fork/05_parallel.cmd
@@ -1,15 +1,13 @@
 #!/bin/bash
-#SBATCH -N 1 
-#SBATCH -n 1
-#SBATCH -c 10 
-#SBATCH --mem=100G
-#SBATCH -t 00:05:00
-
-# This uses the class reservation and will only work for our 
-# class today, in the future, remove this from your SBATCH
-# SBATCH -p class
-
+#SBATCH --job-name=R-serial      # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=10              # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-user=<YourNetID>@princeton.edu
 
 module load openmpi/gcc/2.1.0/64 
 
-Rscript 05_parallel.R
+srun Rscript 05_parallel.R
