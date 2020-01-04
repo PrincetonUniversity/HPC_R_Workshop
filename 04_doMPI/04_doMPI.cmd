bf3d7ad375ceaaffa8c6747d2def5208f1dd7830
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-01-04 17:25:02 -0500

    Update 04_doMPI.cmd

--- 04_doMPI/04_doMPI.cmd
+++ 04_doMPI/04_doMPI.cmd
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=R-mpi         # create a short name for your job
 #SBATCH --nodes=2                # node count
-#SBATCH --ntasks=4               # total number of tasks across all nodes
+#SBATCH --ntasks-per-node=2      # total number of tasks per node
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
 # This is a somewhat inane setup, as it specifies 
