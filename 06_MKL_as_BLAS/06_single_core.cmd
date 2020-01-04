6d37fb796b5363f8263bf2be7549cb4fc88fe74a
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-01-04 17:27:08 -0500

    Update 06_single_core.cmd

--- 06_MKL_as_BLAS/06_single_core.cmd
+++ 06_MKL_as_BLAS/06_single_core.cmd
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
-srun Rscript test.R
+Rscript test.R
