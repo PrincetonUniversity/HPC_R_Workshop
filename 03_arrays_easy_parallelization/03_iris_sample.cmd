c1339242f896bd657811036084fb597d6144972a
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-01-04 17:23:19 -0500

    Update 03_iris_sample.cmd

--- 03_arrays_easy_parallelization/03_iris_sample.cmd
+++ 03_arrays_easy_parallelization/03_iris_sample.cmd
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --array=1-3              # job array with index values 1, 2, 3
-#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-type=all          # send email on job start, end and fault
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
-srun Rscript 03_iris.R $SLURM_ARRAY_TASK_ID
+Rscript 03_iris.R $SLURM_ARRAY_TASK_ID
