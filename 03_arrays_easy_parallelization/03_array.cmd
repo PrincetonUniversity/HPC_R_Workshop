3fec77f7f26943028607a48fed0b965bf0778a5e
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-04-23 22:19:21 -0400

    Update 03_array.cmd

--- 03_arrays_easy_parallelization/03_array.cmd
+++ 03_arrays_easy_parallelization/03_array.cmd
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --mem=4G                 # total memory per node (4G is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --array=0-5              # job array with index values 0, 1, 2, 3, 4, 5
-#SBATCH --mail-type=all          # send email on start, end and fault
+#SBATCH --mail-type=all          # send email on start, end and fail
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
-srun Rscript array.R
+Rscript array.R
