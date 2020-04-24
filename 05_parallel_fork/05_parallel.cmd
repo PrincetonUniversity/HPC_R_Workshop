56107ec6bb0d870d1a98eac58eacafe62492deea
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-04-23 22:22:12 -0400

    Update 05_parallel.cmd

--- 05_parallel_fork/05_parallel.cmd
+++ 05_parallel_fork/05_parallel.cmd
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=10       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on job start, end and fault
+#SBATCH --mail-type=all          # send email on job start, end and fail
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
 module load openmpi/gcc/2.1.0/64 
