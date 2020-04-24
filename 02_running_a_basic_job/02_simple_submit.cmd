5142fb6db8ebc4b3f0ae1aafeb2906e06f3d4665
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-04-23 22:13:17 -0400

    Update 02_simple_submit.cmd

--- 02_running_a_basic_job/02_simple_submit.cmd
+++ 02_running_a_basic_job/02_simple_submit.cmd
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
+#SBATCH --mem=4G                 # total memory per node (4G per cpu-core is default)
 #SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on job start, end and fault
+#SBATCH --mail-type=all          # send email on job start, end and fail
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
 Rscript Hello.R
