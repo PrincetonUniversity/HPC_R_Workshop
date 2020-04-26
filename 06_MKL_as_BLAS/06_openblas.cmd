d793fd09a795be6245b13d49f97b1c0c95ef3094
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2020-04-26 02:32:27 -0400

    Update openblas example

--- 06_MKL_as_BLAS/06_openblas.cmd
+++ 06_MKL_as_BLAS/06_openblas.cmd
@@ -2,7 +2,7 @@
 #SBATCH --job-name=R-mkl         # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=all          # send email on job start, end and fault
