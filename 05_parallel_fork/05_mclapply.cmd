c7ad7d140a47e8ae7e0225ef99934bc550983ad9
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-10-07 11:35:25 -0400

    Update 05_mclapply.cmd

--- 05_parallel_fork/05_mclapply.cmd
+++ 05_parallel_fork/05_mclapply.cmd
@@ -8,6 +8,6 @@
 #SBATCH --mail-type=all          # send email on job start, end and fail
 
 module purge
-module load R/4.3.0
+module load R/4.4.0  # R/4.4.1 on della (R/4.4.0 on adroit)
 
 srun Rscript 05_mclapply.R
