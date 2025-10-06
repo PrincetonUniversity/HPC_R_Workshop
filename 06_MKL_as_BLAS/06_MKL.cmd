ccad2f0699d3bc2b33e431486c634968346aff4f
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-10-05 21:48:42 -0400

    Update 06_MKL.cmd

--- 06_MKL_as_BLAS/06_MKL.cmd
+++ 06_MKL_as_BLAS/06_MKL.cmd
@@ -5,15 +5,15 @@
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on job start, end and fault
+#SBATCH --mail-type=all          # receive email on job start, end, and fault
 
 module purge
-module load R/4.4.1  # R/4.4.2 on della (R/4.4.1 on adroit)
+module load R/4.5.1
 
 # This sets the intel-mkl to use the GNU interface, which 
 # is more compatible with R
 export  MKL_INTERFACE_LAYER=GNU,LP64
 export  MKL_THREADING_LAYER=GNU
 
-module load intel-mkl/2020.1
+module load intel-mkl/2024.2
 LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_rt.so srun Rscript test.R
