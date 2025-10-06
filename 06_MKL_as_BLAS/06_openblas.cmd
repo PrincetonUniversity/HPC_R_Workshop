2deeff4136e6101e7d00282ae58f4a9b97c7041d
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-10-05 21:47:31 -0400

    Update 06_openblas.cmd

--- 06_MKL_as_BLAS/06_openblas.cmd
+++ 06_MKL_as_BLAS/06_openblas.cmd
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=all          # send email on job start, end and fault
+#SBATCH --mail-type=all          # receive email on job start, end, and fault
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 
 module purge
-module load R/4.4.1  # R/4.4.2 on della (R/4.4.1 on adroit)
+module load R/4.5.1
 
 Rscript test.R
