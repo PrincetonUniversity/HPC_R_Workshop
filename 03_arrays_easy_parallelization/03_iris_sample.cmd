98537e59d9499261ff3855ca7c2005dc0b9adb46
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2025-10-05 21:41:49 -0400

    Update 03_iris_sample.cmd

--- 03_arrays_easy_parallelization/03_iris_sample.cmd
+++ 03_arrays_easy_parallelization/03_iris_sample.cmd
@@ -6,9 +6,9 @@
 #SBATCH --mem=4G                 # total memory per node (4G per cpu-core is default)
 #SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 #SBATCH --array=1-3              # job array with index values 1, 2, 3
-#SBATCH --mail-type=all          # send email on job start, end and fail
+#SBATCH --mail-type=all          # receive email on job start, end, and fail
 
 module purge
-module load R/4.4.1  # R/4.4.2 on della (R/4.4.1 on adroit)
+module load R/4.5.1
 
 Rscript 03_iris.R $SLURM_ARRAY_TASK_ID
